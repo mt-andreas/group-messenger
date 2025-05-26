8977dcab7f3aa3cdd1361b5712c559e10463097b
Andreas <mtandreaswork@gmail.com>
2025-05-26 12:10:36 +0200

    register auth api

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import Fastify from "fastify";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
 import jwtPlugin from "./plugins/jwt.js";
+import auth from "./routes/auth.js";
 const app = Fastify({ logger: true });
 
 // Swagger setup
@@ -20,6 +21,7 @@ await app.register(fastifySwaggerUi, {
 
 // Load routes, plugins, etc. here
 await app.register(jwtPlugin);
+await app.register(auth, { prefix: "/auth" });
 
 app.listen({ port: 3000 }, (err) => {
   if (err) {
