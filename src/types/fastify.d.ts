c5ff92353bfab82201449b5641bf351257c4819f
Andreas <mtandreaswork@gmail.com>
2025-05-26 16:12:52 +0200

    fastify authenticate type declaration

---
+++ src/types/fastify.d.ts
@@ -0,0 +1,18 @@
+// src/types/fastify.d.ts
+import "fastify";
+
+declare module "fastify" {
+  interface FastifyInstance {
+    authenticate: (
+      request: FastifyRequest,
+      reply: FastifyReply
+    ) => Promise<void>;
+  }
+
+  interface FastifyRequest {
+    user?: {
+      id: number;
+      email: string;
+    };
+  }
+}
