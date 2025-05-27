df327ee9c8193c94648762c90de97ce8a7f769ac
Andreas <mtandreaswork@gmail.com>
2025-05-27 12:49:07 +0200

    manage websocket connections

---
+++ src/ws/groupSocketManager.ts
@@ -0,0 +1,42 @@
+import { type WebSocket } from "@fastify/websocket";
+import { Message } from "types/messages";
+
+type GroupId = string;
+
+type ClientInfo = {
+  userId: string;
+  socket: WebSocket;
+};
+
+const groupClients = new Map<GroupId, Set<ClientInfo>>();
+
+export function addClient(groupId: string, client: ClientInfo) {
+  if (!groupClients.has(groupId)) {
+    groupClients.set(groupId, new Set());
+  }
+  groupClients.get(groupId)!.add(client);
+}
+
+export function removeClient(groupId: string, socket: WebSocket) {
+  const clients = groupClients.get(groupId);
+  if (clients) {
+    for (const client of clients) {
+      if (client.socket === socket) {
+        clients.delete(client);
+        break;
+      }
+    }
+    if (clients.size === 0) {
+      groupClients.delete(groupId);
+    }
+  }
+}
+
+export function broadcastToGroup(groupId: string, message: Message) {
+  const clients = groupClients.get(groupId);
+  if (clients) {
+    for (const client of clients) {
+      client.socket.send(JSON.stringify(message));
+    }
+  }
+}
