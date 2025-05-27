2c766ecd8fdc2f9485ae870080e47d0a46dd271c
Andreas <mtandreaswork@gmail.com>
2025-05-27 14:41:34 +0200

    change encryption key to be constant

--- src/utils/encryption.ts
+++ src/utils/encryption.ts
@@ -1,6 +1,6 @@
 import crypto from "crypto";
 
-const ENCRYPTION_KEY = crypto.randomBytes(16); // Replace with env/config in real use
+const ENCRYPTION_KEY = Buffer.from("0123456789abcdef0123456789abcdef", "hex"); // Replace with env/config in real use
 const IV_LENGTH = 16;
 
 export function encrypt(text: string): string {
