5be9455470e39136fcaca853a5bc08efe7dc755d
Andreas <mtandreaswork@gmail.com>
2025-05-28 09:52:02 +0200

    add env config for lockout

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,4 @@
 export default {
-  lockoutHours: 48,
+  lockoutHours: process.env.ENCRYPTION_KEY || 48,
   encryptionKey: process.env.ENCRYPTION_KEY || "0123456789abcdef0123456789abcdef", // 32 hex chars for AES-256
 };
